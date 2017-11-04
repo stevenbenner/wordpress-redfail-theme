b9f6ef3cc30e3c62ce0a1bafdb4cc6cfac109f51
Steven Benner <admin@stevenbenner.com>
2017-11-04 16:27:45 -0700

    Added subresource integrity data to cdn hosted jQuery reference

--- footer.php
+++ footer.php
@@ -26,7 +26,7 @@
 <?php wp_footer(); ?>
 
 	<!-- global scripts -->
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" crossorigin="anonymous"></script>
 	<?php versioned_javascript($GLOBALS['TEMPLATE_RELATIVE_URL'].'js/scripts.js'); ?>
 
 </body>
