d98f54c8bf5feffdebeb7dbf23edc5948f841083
Steven Benner <admin@stevenbenner.com>
2022-04-23 06:34:52 -0700

    Remove google analytics

--- footer.php
+++ footer.php
@@ -25,10 +25,6 @@
 
 <?php wp_footer(); ?>
 
-	<!-- global scripts -->
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-xBuQ/xzmlsLoJpyjoggmTEz8OWUFM0/RC5BsqQBDX2v5cMvDHcMakNTNrHIW2I5f" crossorigin="anonymous"></script>
-	<?php versioned_javascript($GLOBALS['TEMPLATE_RELATIVE_URL'].'js/scripts.js'); ?>
-
 </body>
 
 <!-- $ <?php echo $_SERVER['REQUEST_URI']; ?> - <?php echo get_num_queries(); ?> - <?php echo timer_stop(0, 5); ?> - <?php echo date(DATE_W3C); ?> $ -->
