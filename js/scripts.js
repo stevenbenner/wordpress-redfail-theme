06c184a174cc7fe2b1d84ac7bfed92e0fe62ffef
Steven Benner <admin@stevenbenner.com>
2022-04-23 05:38:54 -0700

    Remove twitter timeline jquery plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,3 @@
-(function($) {
-	'use strict';
-
-	var userRegex = /(^|[\W])@(\w+)/gi,
-		hashRegex = /(?:^| )[\#]+([\w\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u00ff\u0600-\u06ff]+)/gi,
-		urlRegex = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi,
-		userMark = '$1<a href="https://twitter.com/$2">@$2</a>',
-		hashMark = ' <a href="https://search.twitter.com/search?q=&tag=$1&lang=all">#$1</a>';
-
-	$.fn.twitterTimeline = function(opts) {
-		var $this = this;
-
-		if (!$this.length) {
-			return $this;
-		}
-
-		var options = $.extend({}, $.fn.twitterTimeline.defaults, opts);
-
-		function pullTimeline() {
-			$.ajax({
-				url: 'https://api.twitter.com/1/statuses/user_timeline.json',
-				dataType: 'jsonp',
-				data: {
-					screen_name: options.username,
-					count: options.count,
-					include_entities: 'true'
-				}
-			}).done(function(data) {
-				var list = $('<ul></ul>', {class: 'twitterTimeline'}),
-					i, count, markedText, postTime;
-				for (i = 0, count = data.length; i < count; i++) {
-					markedText = data[i].text;
-					markedText = markedText.replace(urlRegex, function(match) {
-						var url = (/^[a-z]+:/i).test(match) ? match : 'http://' + match,
-							text = match,
-							n, count, entity;
-						for(n = 0, count = data[i].entities.urls.length; n < count; ++n) {
-							entity = data[i].entities.urls[n];
-							if (entity.url === url && entity.expanded_url) {
-								url = entity.expanded_url;
-								text = entity.display_url;
-								break;
-							}
-						}
-						return '<a href="' + url.replace(/</g, '&lt;').replace(/>/g, '^&gt;') + '">' + text.replace(/</g,"&lt;").replace(/>/g,"^&gt;") + '</a>';
-					});
-					markedText = markedText.replace(userRegex, userMark);
-					markedText = markedText.replace(hashRegex, hashMark);
-					list.append($('<li>' + markedText + '</li>'));
-				}
-
-				$this.empty();
-				$this.append(list);
-			});
-		}
-
-		pullTimeline();
-
-		if ($this.data('twitterInterval')) {
-			clearInterval($this.data('twitterInterval'));
-		}
-
-		$this.data('twitterInterval', setInterval(pullTimeline, options.pollInterval));
-
-		return $this;
-	};
-
-	$.fn.twitterTimeline.defaults = {
-		username: 'stevenbenner',
-		count: 3,
-		pollInterval: 1000 * 60 * 5
-	};
-
-}(jQuery))
-
 var _gaq = [['_setAccount', 'UA-327'+'5918-2'], ['_trackPageview']];
 
 $(function() {
@@ -83,9 +8,6 @@ $(function() {
 	ga.async = true;
 	ga.src = 'https://www.google-analytics.com/ga.js';
 	s.parentNode.insertBefore(ga, s);
-
-	// twitter timeline
-	$('#tweets').twitterTimeline();
 
 	// ga click tracking
 	$('#social-links a').on('click', function() {
